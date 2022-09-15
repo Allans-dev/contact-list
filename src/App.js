7845434b99345f3ff2c14190ab5d80749da2def3
Allan <allanswebdev@gmail.com>
2022-09-15 15:36:52 +1000

    fetch complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
+import ContactView from "./ContactView";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [contactList, setContactList] = useState([]);
+
+  async function getContacts() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const json = await response.json();
+    setContactList(json);
+    return json;
+  }
+
+  useEffect(() => {
+    try {
+      getContacts();
+    } catch {
+      throw Error("error in obtaining contacts");
+    }
+  }, []);
+
+  return (
+    <main className="App">
+      {contactList.map((value, index) => (
+        <ContactView detail={value} index={index} key={index} />
+      ))}
+    </main>
+  );
 }
 
 export default App;
---
+++ src/ContactView.jsx
@@ -0,0 +1,6 @@
+const ContactView = ({ detail, index }) => {
+  console.log(detail);
+  return <div>{detail.username}</div>;
+};
+
+export default ContactView;
