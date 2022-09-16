0d80806c43f69292a0620f1acd9f9f40730389d5
Allan <allanswebdev@gmail.com>
2022-09-16 14:16:27 +1000

    logic for dynamic display complete

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   async function getContacts() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const json = await response.json();
-    setContactList(json);
+    setContactList(() => json);
     return json;
   }
 
--- src/ContactView.jsx
+++ src/ContactView.jsx
@@ -1,31 +1,89 @@
+import { useState } from "react";
 import "./contact-view-styles.css";
 
 const ContactView = ({ detail, index }) => {
   console.log(detail);
 
+  const BUSINESS = "business";
+  const ADDRESS = "address";
+  const START = "start";
+
+  const [id, setId] = useState(1);
+  const [toggle, setToggle] = useState(START);
+
+  const handleBusiness = () => {
+    setId(() => detail.id);
+    setToggle((prev) => (prev === BUSINESS ? START : BUSINESS));
+  };
+  const handleAddress = () => {
+    setId(() => detail.id);
+    setToggle((prev) => (prev === ADDRESS ? START : ADDRESS));
+  };
+
   return (
     <section className="contact">
       <table>
-        <tr>
-          <td colSpan={2}>{detail.name}</td>
-        </tr>
-        <tr>
-          <td>Username</td>
-          <td>{detail.username}</td>
-        </tr>
-        <tr>
-          <td colSpan={2}>{detail.email}</td>
-        </tr>
-        <tr>
-          <td colSpan={2}>{detail.phone}</td>
-        </tr>
-        <tr>
-          <td colSpan={2}>{detail.website}</td>
-        </tr>
+        {toggle === START && (
+          <tbody>
+            <tr>
+              <td colSpan={2}>{detail.name}</td>
+            </tr>
+            <tr>
+              <td>Username:</td>
+              <td>{detail.username}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.email}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.phone}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.website}</td>
+            </tr>
+          </tbody>
+        )}
+        {toggle === BUSINESS && id === detail.id && (
+          <tbody>
+            <tr>
+              <td colSpan={2}>{detail.name}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.company.name}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.company.bs}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.company.catchPhrase}</td>
+            </tr>
+          </tbody>
+        )}
+        {toggle === ADDRESS && id === detail.id && (
+          <tbody>
+            <tr>
+              <td colSpan={2}>{detail.name}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.address.suite}</td>
+            </tr>
+            <tr>
+              <td colSpan={2}>{detail.address.street}</td>
+            </tr>
+            <tr>
+              <td>{detail.address.city}</td>
+              <td>{detail.address.zipcode}</td>
+            </tr>
+          </tbody>
+        )}
       </table>
       <section className="btn-container">
-        <div className="business">💻</div>
-        <div className="address">🏠</div>
+        <div className="business" onClick={handleBusiness}>
+          💻
+        </div>
+        <div className="address" onClick={handleAddress}>
+          🏠
+        </div>
       </section>
     </section>
   );
